61e5dab4f3038b691f768d5ffd609bb55c8ecfce
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2023-12-18 23:30:10 +0330

    feat: implement sign-in & sign-up page

---
+++ src/routes/signin/+page.svelte
@@ -0,0 +1,27 @@
+<main class="flex min-h-screen items-center justify-center">
+	<div class="flex w-80 flex-col items-center justify-center gap-6 rounded-xl border border-slate-700 p-7">
+		<h1 class="text-2xl font-semibold">Welcome Back!</h1>
+
+		<form class="flex w-full flex-col gap-3">
+			<label class="form-control w-full max-w-xs">
+				<div class="label">
+					<span class="label-text">EMail</span>
+				</div>
+				<input type="email" placeholder="username@gmail.com" class="input input-bordered w-full max-w-xs" />
+			</label>
+
+			<label class="form-control w-full max-w-xs">
+				<div class="label">
+					<span class="label-text">Password</span>
+				</div>
+				<input type="password" placeholder="********" class="input input-bordered w-full max-w-xs" />
+			</label>
+
+			<div class="flex justify-end">
+				<button class="btn btn-primary">Sign In</button>
+			</div>
+		</form>
+
+		<p>Don't have an account? <a class="text-secondary" href="/signup">Sign Up!</a></p>
+	</div>
+</main>
