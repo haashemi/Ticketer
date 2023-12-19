75543023cdcc353e6ae5f8f57949dbcef4cf1fc8
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2023-12-19 19:28:48 +0330

    feat(frontend): finalize the homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,46 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
 <header class="sticky top-0 z-50 w-full bg-black/10 backdrop-blur-lg">
 	<header class="container mx-auto flex h-16 items-center justify-between px-5">
 		<div class="flex gap-2">
 			<a href="/signin" class="btn btn-secondary">Login</a>
 		</div>
 
-		<h1 class="text-xl font-bold text-white">Ticketer</h1>
+		<a href="/" class="font-silk text-2xl font-bold text-white">Ticketer</a>
 	</header>
 </header>
 
-<main class="container mx-auto">
+<main class="container mx-auto px-5">
+	<!-- Header -->
 	<div class="my-14 flex w-full flex-col items-center gap-3">
-		<h2 class="text-4xl font-semibold italic">#1 Event ticket store</h2>
+		<h2 class="text-4xl font-semibold italic text-slate-50">#1 Event ticket store</h2>
 		<p class="text-sm text-slate-500">Get the newest events' ticket from Ticketer!</p>
 	</div>
 
-	<h3 class="my-3 text-lg italic">Latest Events:</h3>
+	<!-- Events list -->
+	<h3 class="font-poppins my-3 text-lg italic">Latest Events:</h3>
+	<div class="grid grid-cols-1 gap-8 lg:grid-cols-2 xl:grid-cols-3">
+		{#each data.events as event}
+			<a
+				href="/event/{event.id}"
+				class="card aspect-video overflow-hidden bg-base-200 bg-cover shadow-md transition-transform ease-in-out hover:-translate-y-2"
+				style="background-image: url('https://cataas.com/cat/cute?id={event.id}');"
+			>
+				<div class="card-body flex flex-col justify-end gap-4 bg-gradient-to-b from-transparent to-black text-white">
+					<h2 class="font-poppins card-title whitespace-nowrap text-2xl italic sm:text-3xl">{event.name}</h2>
 
-	<div class="grid grid-cols-3 gap-5">
-		{#each Array(20) as idx}
-			<div class="card bg-base-200 shadow-md">
-				<figure><img src="https://cataas.com/cat/cute" class="aspect-video" alt="Cat" /></figure>
-
-				<div class="card-body">
-					<h2 class="card-title">کنسرت عمه فاحشه ات</h2>
-					<p>Your عمه جنده's concert begins at 1402/11/22 20:30 in Qazvin</p>
-
-					<div class="card-actions justify-end pt-2">
-						<button class="btn btn-accent">Book a Ticket</button>
+					<div class="card-actions items-center justify-between">
+						<p class="text-base text-accent sm:text-lg">{event.price} IRT</p>
+						<button class="btn btn-accent btn-sm">Book a Ticket</button>
 					</div>
 				</div>
-			</div>
+			</a>
 		{/each}
 	</div>
 </main>
 
-<footer class="border-base-300 mt-10 flex h-11 items-center justify-center border-t">Copyright Ticketer, no rights reserved at all.</footer>
+<footer class="mt-10 flex h-11 items-center justify-center border-t border-base-300">Copyright Ticketer, no rights reserved at all.</footer>
---
+++ src/routes/+page.ts
@@ -0,0 +1,24 @@
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async () => {
+	return {
+		events: [
+			{ id: 1, name: 'My Random Shit', price: 69_000 },
+			{ id: 2, name: 'Yet Another', price: 69_000 },
+			{ id: 3, name: "Oh there's more!", price: 69_000 },
+			{ id: 4, name: "The Weekend's Concert", price: 69_000 },
+			{ id: 5, name: 'My Random Shit', price: 69_000 },
+			{ id: 6, name: 'Yet Another', price: 69_000 },
+			{ id: 7, name: "Oh there's more!", price: 69_000 },
+			{ id: 8, name: "The Weekend's Concert", price: 69_000 },
+			{ id: 9, name: 'My Random Shit', price: 69_000 },
+			{ id: 10, name: 'Yet Another', price: 69_000 },
+			{ id: 11, name: "Oh there's more!", price: 69_000 },
+			{ id: 12, name: "The Weekend's Concert", price: 69_000 },
+			{ id: 13, name: 'My Random Shit', price: 69_000 },
+			{ id: 14, name: 'Yet Another', price: 69_000 },
+			{ id: 15, name: "Oh there's more!", price: 69_000 },
+			{ id: 16, name: "The Weekend's Concert", price: 69_000 },
+		],
+	};
+};
