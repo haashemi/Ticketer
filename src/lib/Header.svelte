e6b43bffe2aa09142ff10f92c262b0c09e1278bc
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2023-12-19 19:31:49 +0330

    chore(frontend): move the Header to a different component

---
+++ src/lib/Header.svelte
@@ -0,0 +1,9 @@
+<header class="sticky top-0 z-50 w-full bg-black/10 backdrop-blur-lg">
+	<header class="container mx-auto flex h-16 items-center justify-between px-5">
+		<div class="flex gap-2">
+			<a href="/signin" class="btn btn-secondary">Login</a>
+		</div>
+
+		<a href="/" class="font-silk text-2xl font-bold text-white">Ticketer</a>
+	</header>
+</header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,11 @@
 <script lang="ts">
+	import Header from '$lib/Header.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 </script>
 
-<header class="sticky top-0 z-50 w-full bg-black/10 backdrop-blur-lg">
-	<header class="container mx-auto flex h-16 items-center justify-between px-5">
-		<div class="flex gap-2">
-			<a href="/signin" class="btn btn-secondary">Login</a>
-		</div>
-
-		<a href="/" class="font-silk text-2xl font-bold text-white">Ticketer</a>
-	</header>
-</header>
+<Header />
 
 <main class="container mx-auto px-5">
 	<!-- Header -->
