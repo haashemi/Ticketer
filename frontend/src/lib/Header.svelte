7d4bd43ec522e1dc8111f579e01525b78ba2ee8a
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2023-12-21 19:39:11 +0330

    chore(frontend): restyle the header

--- frontend/src/lib/Header.svelte
+++ frontend/src/lib/Header.svelte
@@ -1,9 +1,10 @@
-<header class="sticky top-0 z-50 w-full bg-black/10 backdrop-blur-lg">
+<header class="from-base-100 sticky top-0 z-20 w-full bg-gradient-to-b from-30% to-transparent">
 	<header class="container mx-auto flex h-16 items-center justify-between px-5">
-		<div class="flex gap-2">
-			<a href="/signin" class="btn btn-secondary">Login</a>
-		</div>
-
 		<a href="/" class="font-silk text-2xl font-bold text-white">Ticketer</a>
+
+		<div class="flex gap-2">
+			<a href="/signup" class="btn btn-ghost min-h-10 h-10 font-medium">Sign Up</a>
+			<a href="/signin" class="btn btn-primary min-h-10 h-10 font-medium">Sign In</a>
+		</div>
 	</header>
 </header>
