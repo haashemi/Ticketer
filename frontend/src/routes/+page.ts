28e7786037e93a6cf262adb42bc8cb69022e51b9
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2023-12-21 19:38:38 +0330

    chore(frontend): rename event(s) to movie(s)

--- frontend/src/routes/+page.ts
+++ frontend/src/routes/+page.ts
@@ -2,23 +2,22 @@ import type { PageLoad } from './$types';
 
 export const load: PageLoad = async () => {
 	return {
-		events: [
-			{ id: 1, name: 'My Random Shit', price: 69_000 },
-			{ id: 2, name: 'Yet Another', price: 69_000 },
-			{ id: 3, name: "Oh there's more!", price: 69_000 },
-			{ id: 4, name: "The Weekend's Concert", price: 69_000 },
-			{ id: 5, name: 'My Random Shit', price: 69_000 },
-			{ id: 6, name: 'Yet Another', price: 69_000 },
-			{ id: 7, name: "Oh there's more!", price: 69_000 },
-			{ id: 8, name: "The Weekend's Concert", price: 69_000 },
-			{ id: 9, name: 'My Random Shit', price: 69_000 },
-			{ id: 10, name: 'Yet Another', price: 69_000 },
-			{ id: 11, name: "Oh there's more!", price: 69_000 },
-			{ id: 12, name: "The Weekend's Concert", price: 69_000 },
-			{ id: 13, name: 'My Random Shit', price: 69_000 },
-			{ id: 14, name: 'Yet Another', price: 69_000 },
-			{ id: 15, name: "Oh there's more!", price: 69_000 },
-			{ id: 16, name: "The Weekend's Concert", price: 69_000 },
+		movies: [
+			{ id: 1, name: 'Movie Name' },
+			{ id: 2, name: 'Movie Name' },
+			{ id: 3, name: 'Movie Name' },
+			{ id: 4, name: 'Movie Name' },
+			{ id: 5, name: 'Movie Name' },
+			{ id: 6, name: 'Movie Name' },
+			{ id: 7, name: 'Movie Name' },
+			{ id: 8, name: 'Movie Name' },
+			{ id: 9, name: 'Movie Name' },
+			{ id: 10, name: 'Movie Name' },
+			{ id: 11, name: 'Movie Name' },
+			{ id: 12, name: 'Movie Name' },
+			{ id: 13, name: 'Movie Name' },
+			{ id: 14, name: 'Movie Name' },
+			{ id: 15, name: 'Movie Name' },
 		],
 	};
 };
