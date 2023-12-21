81e1eeb876751604e1eb892fdb790ef816d69645
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2023-12-21 19:31:40 +0330

    feat(frontend): add daisyUi theme

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -11,14 +11,30 @@ export default {
 	},
 
 	daisyui: {
-		themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+		themes: [
+			{
+				light: {
+					primary: '#302470',
+					secondary: '#8776db',
+					accent: '#3f26c0',
+					neutral: '#1b1538',
+					'base-100': '#eeecf8',
+				},
+				dark: {
+					primary: '#9a8eda',
+					secondary: '#342489',
+					accent: '#5940d9',
+					neutral: '#1b1538',
+					'base-100': '#070708',
+				},
+			},
+		],
+
 		darkTheme: 'dark', // name of one of the included themes for dark mode
 		base: true, // applies background color and foreground color for root element by default
 		styled: true, // include daisyUI colors and design decisions for all components
 		utils: true, // adds responsive and modifier utility classes
-		prefix: '', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
 		logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-		themeRoot: ':root', // The element that receives theme color CSS variables
 	},
 
 	plugins: [require('daisyui')],
