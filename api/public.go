05e6bb32563fa226339bbb329cf5236cdaefa057
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2023-12-31 19:40:11 +0330

    feat(backend): implement GetMovie

--- api/public.go
+++ api/public.go
@@ -16,5 +16,17 @@ func (a *API) GetMovies(ctx iris.Context) {
 }
 
 func (a *API) GetMovie(ctx iris.Context) {
+	mid, err := ctx.Params().GetInt64("id")
+	if err != nil {
+		ctx.StopWithJSON(iris.StatusBadRequest, NewError("Request is not valid", err))
+		return
+	}
 
+	movie, err := sql.SelectMovie(ctx, a.db, mid)
+	if err != nil {
+		ctx.StopWithJSON(iris.StatusInternalServerError, NewError("Failed to fetch movie, please try again later.", err))
+		return
+	}
+
+	ctx.JSON(movie)
 }
--- sql/movies.go
+++ sql/movies.go
@@ -19,9 +19,13 @@ func SelectMovies(ctx context.Context, db Querier) ([]MovieSummary, error) {
 }
 
 type Movie struct {
-	ID            int64
-	Name          string
-	Time          int16
-	Genres        []string
-	ReservedSeats []int16
+	ID     int64    `db:"id" json:"id"`
+	Name   string   `db:"name" json:"name"`
+	Time   int16    `db:"movie_time" json:"time"`
+	Genres []string `db:"genres" json:"genres"`
+}
+
+func SelectMovie(ctx context.Context, db Querier, id int64) (data Movie, err error) {
+	err = pgxscan.Get(ctx, db, &data, `select id, name, movie_time, genres from movies where id = $1`, id)
+	return
 }
