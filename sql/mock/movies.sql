56d80e1b48d2d47a5e7745c0d33e2c66f498bffd
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2023-12-31 19:06:29 +0330

    feat(backend/sql): add mocked movies

---
+++ sql/mock/movies.sql
@@ -0,0 +1,14 @@
+insert into movies (show_time, movie_time, name, genres)
+    values 
+        (0  , 65 , 'Residency'                  , ARRAY ['Documentary']),
+        (65 , 62 , 'The Lost Of The Whale Shark', ARRAY ['Documentary']),
+        (130, 85 , 'Bache Zerang'               , ARRAY ['Adventure', 'Kids and teenager']),
+        (215, 71 , 'Dadars'                     , ARRAY ['Documentary']),
+        (286, 101, 'Hedgehog'                   , ARRAY ['Comedy']),
+        (390, 96 , 'Luca'                       , ARRAY ['Kids and teenager']),
+        (486, 60 , 'Screenshot'                 , ARRAY ['Theater']),
+        (546, 75 , 'Human/Horse, Fifty/Fifty'   , ARRAY ['Theater']),
+        (621, 60 , 'Melancholy'                 , ARRAY ['Theater']),
+        (681, 68 , 'Captive Breeding'           , ARRAY ['Documentary']),
+        (750, 74 , 'Parvande Tamarrode Man'     , ARRAY ['Documentary']),
+        (830, 97 , 'Khallaj Family'             , ARRAY ['Documentary'])
