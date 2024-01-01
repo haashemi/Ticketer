764f9e36db4a3736563c7e566ea6d66951daf440
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2024-01-02 01:02:31 +0330

    chore(backend/sql): update the movies mock

--- sql/mock/movies.sql
+++ sql/mock/movies.sql
@@ -1,14 +1,14 @@
-insert into movies (show_time, movie_time, name, genres)
+insert into movies (name, movie_time, genres, from_date, to_date, premiere_time)
     values 
-        (0  , 65 , 'Residency'                  , ARRAY ['Documentary']),
-        (65 , 62 , 'The Lost Of The Whale Shark', ARRAY ['Documentary']),
-        (130, 85 , 'Bache Zerang'               , ARRAY ['Adventure', 'Kids and teenager']),
-        (215, 71 , 'Dadars'                     , ARRAY ['Documentary']),
-        (286, 101, 'Hedgehog'                   , ARRAY ['Comedy']),
-        (390, 96 , 'Luca'                       , ARRAY ['Kids and teenager']),
-        (486, 60 , 'Screenshot'                 , ARRAY ['Theater']),
-        (546, 75 , 'Human/Horse, Fifty/Fifty'   , ARRAY ['Theater']),
-        (621, 60 , 'Melancholy'                 , ARRAY ['Theater']),
-        (681, 68 , 'Captive Breeding'           , ARRAY ['Documentary']),
-        (750, 74 , 'Parvande Tamarrode Man'     , ARRAY ['Documentary']),
-        (830, 97 , 'Khallaj Family'             , ARRAY ['Documentary'])
+        ('Residency'                  , 65 , ARRAY ['Documentary']      , '2024-01-01', '2025-01-01', '04:30:00'),
+        ('The Lost Of The Whale Shark', 62 , ARRAY ['Documentary']      , '2024-01-01', '2025-01-01', '05:35:00'),
+        ('Bache Zerang'               , 85 , ARRAY ['Adventure', 'Kids'], '2024-01-01', '2025-01-01', '06:40:00'),
+        ('Dadars'                     , 71 , ARRAY ['Documentary']      , '2024-01-01', '2025-01-01', '08:15:00'),
+        ('Hedgehog'                   , 101, ARRAY ['Comedy']           , '2024-01-01', '2025-01-01', '09:26:00'),
+        ('Luca'                       , 96 , ARRAY ['Kids']             , '2024-01-01', '2025-01-01', '11:07:00'),
+        ('Screenshot'                 , 60 , ARRAY ['Theater']          , '2024-01-01', '2025-01-01', '12:43:00'),
+        ('Human/Horse, Fifty/Fifty'   , 75 , ARRAY ['Theater']          , '2024-01-01', '2025-01-01', '13:43:00'),
+        ('Melancholy'                 , 60 , ARRAY ['Theater']          , '2024-01-01', '2025-01-01', '15:00:00'),
+        ('Captive Breeding'           , 68 , ARRAY ['Documentary']      , '2024-01-01', '2025-01-01', '16:00:00'),
+        ('Parvande Tamarrode Man'     , 74 , ARRAY ['Documentary']      , '2024-01-01', '2025-01-01', '17:10:00'),
+        ('Khallaj Family'             , 97 , ARRAY ['Documentary']      , '2024-01-01', '2025-01-01', '18:25:00');
