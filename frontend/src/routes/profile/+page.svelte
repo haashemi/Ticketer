50b0e508acb3bab69b1f1f558d9b6c494fd1aabf
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2024-01-12 00:49:05 +0330

    feat(frontend): implement createQuery for get ticket summary

--- frontend/src/routes/profile/+page.svelte
+++ frontend/src/routes/profile/+page.svelte
@@ -1,10 +1,29 @@
 <script lang="ts">
 	import Container from '$lib/Container.svelte';
 	import moment from 'moment';
-	import type { PageData } from './$types';
 	import Header from '$lib/Header.svelte';
+	import { createQuery } from '@tanstack/svelte-query';
+	import * as z from 'zod';
+	import ky from 'ky';
 
-	export let data: PageData;
+	const schema = z.object({
+		tickets: z.array(
+			z.object({
+				movieID: z.number(),
+				ticketID: z.number(),
+				movieName: z.string(),
+				premiereTime: z.number(),
+			}),
+		),
+	});
+
+	const query = createQuery({
+		queryKey: ['tickets'],
+		queryFn: async () => {
+			const data = await ky.get('/api/profile/tickets').json();
+			return schema.parse(data);
+		},
+	});
 </script>
 
 <Header />
@@ -14,16 +33,29 @@
 <Container class="font-poppins relative mb-36 mt-20 flex flex-col gap-5">
 	<h1 class="mb-10 text-center">Purchased Tickets</h1>
 
-	<div class="grid grid-cols-1 gap-5 lg:grid-cols-3">
-		{#each data.tickets as ticket}
-			<div class="flex w-full justify-between rounded-xl border border-zinc-700 bg-zinc-800/75 p-3">
-				<div class="flex flex-col justify-around">
-					<p>{ticket.movieName}</p>
-					<p>{moment(ticket.premieresAt).format('MMMM Do YYYY, HH:mm')}</p>
-				</div>
+	{#if $query.isLoading}
+		<div class="grid grid-cols-1 gap-5 lg:grid-cols-3">
+			{#each new Array(10) as _}
+				<div class="flex h-20 w-full animate-pulse justify-between rounded-xl border border-zinc-700 bg-zinc-800/75 p-3" />
+			{/each}
+		</div>
+	{:else if $query.isSuccess}
+		{@const tickets = $query.data.tickets}
+		{#if tickets.length === 0}
+			<div class="text-center">You don't have any tickets reserved</div>
+		{:else}
+			<div class="grid grid-cols-1 gap-5 lg:grid-cols-3">
+				{#each tickets as ticket}
+					<div class="flex h-20 w-full justify-between rounded-xl border border-zinc-700 bg-zinc-800/75 p-3">
+						<div class="flex flex-col justify-around">
+							<p>{ticket.movieName}</p>
+							<p>{moment(new Date(ticket.premiereTime)).format('MMMM Do YYYY, HH:mm')}</p>
+						</div>
 
-				<a href="/ticket/{ticket.id}" class="btn">View</a>
+						<a href="/ticket/{ticket.ticketID}" class="btn">View</a>
+					</div>
+				{/each}
 			</div>
-		{/each}
-	</div>
+		{/if}
+	{/if}
 </Container>
