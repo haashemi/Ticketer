11016ff1667bb39261e4552a74dbdc8cce49424d
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2024-01-13 02:56:30 +0330

    feat(backend/sql): add InsertTicket, InsertTicket and fix some queries

--- sql/tickets.go
+++ sql/tickets.go
@@ -5,30 +5,31 @@ import (
 	"time"
 
 	"github.com/georgysavva/scany/v2/pgxscan"
+	"github.com/jackc/pgx/v5/pgtype"
 )
 
 type Ticket struct {
-	MovieID      int64     `db:"movie_id" json:"movieId"`
-	MovieName    string    `db:"movie_name" json:"movieName"`
-	MovieTime    int8      `db:"movie_time" json:"movieTime"`
-	MovieGenres  []string  `db:"movie_genres" json:"movieGenres"`
-	ReservedAt   time.Time `db:"reserved_at" json:"reservedAt"`
-	PremiereDate time.Time `db:"premiere_date" json:"premiereDate"`
-	PremiereTime Time      `db:"premiere_time" json:"premiereTime"`
-	// ReservedSeats []int8    `db:"reserved_seats" json:"reservedSeats"`
+	MovieID       int64     `db:"movie_id" json:"movieId"`
+	MovieName     string    `db:"movie_name" json:"movieName"`
+	MovieTime     int8      `db:"movie_time" json:"movieTime"`
+	MovieGenres   []string  `db:"movie_genres" json:"movieGenres"`
+	ReservedAt    time.Time `db:"reserved_at" json:"reservedAt"`
+	PremiereDate  time.Time `db:"premiere_date" json:"premiereDate"`
+	PremiereTime  *Time     `db:"premiere_time" json:"premiereTime"`
+	ReservedSeats []int8    `db:"reserved_seats" json:"reservedSeats"`
 }
 
 func SelectTicket(ctx context.Context, db Querier, ticketID, userID int64) (data Ticket, err error) {
-	err = pgxscan.Select(ctx, db, &data, `
+	err = pgxscan.Get(ctx, db, &data, `
 		select 
-			m.id			as movie_id,
-			m.name 			as movie_name,
-			m.movie_time 	as movie_time,
-			m.genres 		as movie_genres,
-			t.reserved_at 	as reserved_at,
-			t.premiere_date as premiere_date,
-			t.premiere_time as premiere_time
-			-- array agg s.seat_number as reserved_seats
+			m.id			as movie_id				,
+			m.name 			as movie_name			,
+			m.movie_time 	as movie_time			,
+			m.genres 		as movie_genres			,
+			t.reserved_at 	as reserved_at			,
+			t.premiere_date as premiere_date		,
+			t.premiere_time as premiere_time		,
+			array_agg(s.seat_number) as reserved_seats
 		from
 			tickets t
 			left join seats s on s.ticket_id = t.id
@@ -37,7 +38,7 @@ func SelectTicket(ctx context.Context, db Querier, ticketID, userID int64) (data
 			t.id = $1
 			and t.user_id = $2
 		group by
-			t.id
+			t.id, m.id
 	 `, ticketID, userID)
 	return
 }
@@ -46,7 +47,8 @@ type TicketSummary struct {
 	MovieID      int64     `db:"movie_id" json:"movieID"`
 	TicketID     int64     `db:"ticket_id" json:"ticketID"`
 	MovieName    string    `db:"movie_name" json:"movieName"`
-	PremiereTime time.Time `db:"premiere_time" json:"premiereTime"`
+	PremiereDate time.Time `db:"premiere_date" json:"premiereDate"`
+	PremiereTime *Time     `db:"premiere_time" json:"premiereTime"`
 }
 
 func SelectUserTickets(ctx context.Context, db Querier, user int64) (data []TicketSummary, err error) {
@@ -56,12 +58,25 @@ func SelectUserTickets(ctx context.Context, db Querier, user int64) (data []Tick
 			m.id 			as movie_id		,
 			t.id 			as ticket_id	,
 			m.name 			as movie_name	,
+			t.premiere_date as premiere_date,
 			t.premiere_time as premiere_time
 		from
 			tickets t
 			join movies m on m.id = t.movie_id
 		where 
 			t.user_id = $1
+		order by
+			reserved_at desc
 	 `, user)
 	return
+}
+
+func InsertTicket(ctx context.Context, db Querier, uid, mid int64, pDate time.Time, pTime pgtype.Time) (id int64, err error) {
+	err = pgxscan.Get(ctx, db, &id, `insert into tickets (user_id, movie_id, premiere_date, premiere_time) values ($1, $2, $3, $4) returning id`, uid, mid, pDate, pTime)
+	return
+}
+
+func InsertSeat(ctx context.Context, db Querier, tid int64, seat int8) error {
+	_, err := db.Exec(ctx, `insert into seats (ticket_id, seat_number) values ($1, $2)`, tid, seat)
+	return err
 }
