65833de93d4bf456f3003b0fdbf010d2e680cbee
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2024-01-13 02:57:24 +0330

    feat(backend): implement PostReserveTickets

--- api/protected.go
+++ api/protected.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"time"
+
 	"github.com/haashemi/Ticketer/sql"
 	"github.com/kataras/iris/v12"
 )
@@ -54,4 +56,59 @@ func (a *API) GetTickets(ctx iris.Context) {
 	ctx.JSON(GetTicketsResponse{Tickets: tickets})
 }
 
-func (a *API) PostReserveTickets(ctx iris.Context) { ctx.StatusCode(iris.StatusNotImplemented) }
+type PostReserveTicketsRequest struct {
+	MovieID  int64  `json:"movieId" validate:"required"`
+	ForYear  int    `json:"forYear" validate:"required"`
+	ForMonth int    `json:"forMonth" validate:"required"`
+	ForDay   int    `json:"forDay" validate:"required"`
+	Seats    []int8 `json:"seats" validate:"required"`
+}
+
+type PostReserveTicketsResponse struct {
+	TicketID int64 `json:"ticketId"`
+}
+
+func (a *API) PostReserveTickets(ctx iris.Context) {
+	var body PostReserveTicketsRequest
+	if err := ctx.ReadJSON(&body); err != nil {
+		ctx.StopWithJSON(iris.StatusBadRequest, NewError("Request is not valid", err))
+		return
+	}
+
+	claims, _ := ctx.Values().Get("claims").(*Claims)
+
+	var tid int64
+	err := a.db.Transaction(ctx, func(tx sql.Querier) error {
+		movie, err := sql.SelectMovie(ctx, tx, body.MovieID)
+		if err != nil {
+			return NewError("Failed to find the movie", err)
+		}
+
+		uu := time.Date(body.ForYear, time.Month(body.ForMonth), body.ForDay, 0, 0, 0, 0, time.UTC)
+
+		tid, err = sql.InsertTicket(ctx, tx, claims.UserID, movie.ID, uu, movie.PremiereTime.Time)
+		if err != nil {
+			return NewError("Failed to get you a ticket", err)
+		}
+
+		for _, seat := range body.Seats {
+			err = sql.InsertSeat(ctx, tx, tid, seat)
+			if err != nil {
+				return NewError("Failed to reserve your seat", err)
+			}
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		if _, ok := err.(Error); ok {
+			ctx.StopWithJSON(iris.StatusInternalServerError, err)
+		} else {
+			ctx.StopWithJSON(iris.StatusInternalServerError, NewError("Failed to reserve, please try again later.", err))
+		}
+		return
+	}
+
+	ctx.JSON(PostReserveTicketsResponse{TicketID: tid})
+}
